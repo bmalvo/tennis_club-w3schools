0a99336786b6e3c44132191dd28edda0e7bfc01f
bmalvo <boydmalvo@gmail.com>
2023-08-06 21:12:23 +0200

    add global static files

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'productionfiles'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'mystaticfiles'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
